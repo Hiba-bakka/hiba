9bf831bb68b99116c525fd8246d2982912d64aac
Hiba-bakka <138728531+Hiba-bakka@users.noreply.github.com>
2024-02-17 22:50:32 +0200

    Add files via upload

---
+++ Protobyte2/SQLQuery3.sql
@@ -0,0 +1,22 @@
+﻿SELECT * FROM users2
+
+CREATE  TABLE  employe
+(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	employee_id VARCHAR(MAX) NULL,
+	full_name VARCHAR(MAX) NULL,
+	gender VARCHAR(MAX) NULL,
+	contact_number VARCHAR(MAX) NULL,
+	postion VARCHAR(MAX) NULL,
+	image VARCHAR(MAX) NULL,
+	salary  INT NULL,
+	insert_date DATE NULL,
+	update_date DATE NULL,
+	delete_date DATE NULL,
+
+)
+
+SELECT * FROM employe
+
+ALTER TABLE employe
+ADD status VARCHAR(MAX) NULL
